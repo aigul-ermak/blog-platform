337ead25b1116be2600a92c4ceb9de304de69507
Sieele <sieele.uchiha@gmail.com>
2024-10-03 15:30:11 -0300

    implementing sidebar

--- project-blog/src/Components/Sidebar.jsx
+++ project-blog/src/Components/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+import { IoChevronBackOutline } from "react-icons/io5";
 
 function Sidebar() {
+
+  const [open, setOpen] = useState(true)
+
   return (
     <div className='flex'>
         
-      <div className={'w-72 h-screen bg-dark-purple'}>Sidebar</div>       
+      <div className={`${open ? "w-72" : "w-20" } duration-300 h-screen bg-dark-purple relative`}>Sidebar
+        
+      <IoChevronBackOutline className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-white ${!open && "rotate-180"}`} 
+        onClick={() => setOpen(!open)}/>
+
+        </div>       
         
     </div>
 
