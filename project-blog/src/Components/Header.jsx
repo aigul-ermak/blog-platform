df8194fa85949655c142a274ba92c85a2c2875a9
Sieele <sieele.uchiha@gmail.com>
2024-10-03 14:48:53 -0300

    pushing

--- project-blog/src/Components/Header.jsx
+++ project-blog/src/Components/Header.jsx
@@ -1,17 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import logo from './../assets/img/logo.png'
+import { IoCreate } from "react-icons/io5";
 
 function Header() {
   return (
-    <div className='flex justify-between items-center '>
+    <div className='flex justify-between items-center'>
         <img src ={logo} className='w-[180px]'/>
         <ul className='flex gap-4 md:gap-14'>
             <li className='hover:font-bold cursor-pointer'>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li className='hover:font-bold cursor-pointer'>About</li>
+            <li className='hover:font-bold cursor-pointer'>Contact</li>
         </ul>
-        <button>Sign Up</button>
+        <button className='bg-dark-purple rounded-full text-white flex items-center'>Sign Up <IoCreate className='ml-3'/> </button>
     </div>
   )
 }
