6e676857ec429771086031b6f518e450fa10496c
Sieele <sieele.uchiha@gmail.com>
2024-10-24 16:05:03 -0300

    testing register and login with Bruno and MySQL

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+@Post('register')  
+async register(
+  @Body('name') name: string,
+  @Body('email') email: string,
+  @Body('password') password: string,
+){
+  
+  const hashedPassword = await  bcrypt.hash(password, 12);
+
+  return this.appService.create({
+    name, 
+    email,
+    password: hashedPassword
+  })
+}
+
+@Post('login')
+async Login(
+  @Body('email') email: string,
+  @Body('password') password: string,
+){
+
+  const user = await this.appService.findOne({ where: {email}});
+
+  if(!user){
+    throw new BadRequestException('Invalid credentials');
   }
+
+  if(!await bcrypt.compare(password, user.password)){
+    throw new BadRequestException('Invalid credentials');
+  }
+
+  return user
+
+}
+
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'devJPA',
+    password: 'Dev1234!',
+    database: 'blog_db',
+    entities: [User],
+    synchronize: true,
+  }),
+
+TypeOrmModule.forFeature([User])
+
+],
   controllers: [AppController],
   providers: [AppService],
 })
